00635649ec2abcc94aa28d9be3f3de340072b731
Becca Kroese <becca@skwintz.com>
2018-05-09 16:35:38 -0500

    Extra pages

--- js/main.js
+++ js/main.js
@@ -18,6 +18,41 @@ $(document).ready(function() {
 	$(".footer .agis").on('click touch', function () {
 	    $(".footer .dropdown").slideToggle();
 	});
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top - 100
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
 });
 
 /*-----------------------------------------------------------------*/
@@ -25,10 +60,10 @@ $(document).ready(function() {
 /*-----------------------------------------------------------------*/
 $(document).ready(function(){			  
 	$(".floatlabel").focus(function(){
-		$(this).parent().addClass("focus");
+		$(this).parent().addClass("infield");
 
 	  }).blur(function(){
-	       $(this).parent().removeClass("focus");
+	       $(this).parent().removeClass("infield");
 	  })
 	function checkForInput(element) {
 	  // element is passed to the function ^
@@ -136,8 +171,6 @@ $(document).ready(function(){
         rules: {
             f_name : "required",
             l_name : "required",
-            email : "required",
-            phone : "required",
             location : "required",
             program : "required"
         },
